480a393510c961b311d83047261f837e55fdab2e
ujiro99 <siro.cola@gmail.com>
2013-10-16 00:36:52 +0900

    fix google analytics using google-analytics-bundle.js
    https://github.com/GoogleChrome/chrome-platform-analytics/wiki

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,27 +1,19 @@
-var _AnalyticsCode = 'UA-32234486-8';
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', _AnalyticsCode]);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
+var AnalyticsCode = 'UA-32234486-8';
+var service = analytics.getService('RedmineTimeTracker');
+var tracker = service.getTracker(AnalyticsCode);
 
 /**
  * Track a click on a button using the asynchronous tracking API.
  */
 function trackButtonClick(e) {
-  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+  tracker.sendEvent('ButtonClick', e.target.id, 'clicked');
 }
 
 /**
  * Set up your event handlers for `button` elements.
  */
 document.addEventListener('DOMContentLoaded', function () {
+  tracker.sendAppView(this.title);
   var buttons = document.querySelectorAll('button');
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', trackButtonClick);
