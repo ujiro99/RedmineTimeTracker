9918a01442c1f410017b33cbbc6c3f4da4646ad4
ujiro99 <siro.cola@gmail.com>
2014-04-29 18:49:09 +0900

    change height.

--- app/scripts/eventPage.js
+++ app/scripts/eventPage.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   return chrome.app.window.create('/views/index.html', {
     'bounds': {
       'width': 250,
-      'height': 380
+      'height': 550
     }
   });
 });
