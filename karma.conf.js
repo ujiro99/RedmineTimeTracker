f93c9cf7ed3ef65271413a9c17d58a6a3a1b54e7
ujiro99 <siro.cola@gmail.com>
2016-11-19 22:43:51 +0900

    Fix settings for Karma and Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -96,7 +96,7 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
 
     // for Travis CI
     customLaunchers: {
