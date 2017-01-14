c97a0d126ae9fffc54a88c92a08b88135dca4ab6
ujiro99 <siro.cola@gmail.com>
2017-01-15 05:58:38 +0900

    Cant't source script.

--- scripts/install_chrome.sh
+++ scripts/install_chrome.sh
@@ -7,7 +7,5 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 else
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome*.deb
-    export CHROME_BIN=/usr/bin/google-chrome
-    export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start
 fi
